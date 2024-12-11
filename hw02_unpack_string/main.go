50183535d91da10e1e331e37863d6bc3f36017b7
Sergey Sobolev <info@hondaworld.ru>
2024-12-12 01:50:52 +0300

    HW2 is completed

---
+++ hw02_unpack_string/main.go
@@ -0,0 +1,5 @@
+package hw02unpackstring
+
+func main() {
+
+}
--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,52 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(str string) (string, error) {
+	var result strings.Builder
+	var lastValue = ""
+	var isSlash = false
+
+	for _, val := range str {
+		value := string(val)
+		num, err := strconv.Atoi(value)
+
+		if err == nil {
+			switch {
+			case lastValue == "":
+				return "", ErrInvalidString
+			case isSlash:
+				result.WriteString(lastValue)
+				lastValue = value
+				isSlash = false
+			default:
+				result.WriteString(strings.Repeat(lastValue, num))
+				lastValue = ""
+			}
+		} else {
+			switch {
+			case isSlash && value != `\`:
+				return "", ErrInvalidString
+			case !isSlash && value == `\`:
+				isSlash = true
+			case isSlash && value == `\`:
+				isSlash = false
+				fallthrough
+			case lastValue != "":
+				result.WriteString(lastValue)
+				fallthrough
+			default:
+				lastValue = value
+			}
+		}
+	}
+
+	result.WriteString(lastValue)
+
+	return result.String(), nil
 }
