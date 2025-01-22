8a5b367a96b0909d00d6e8e17ff9f7deeeeb6e40
Sergey Sobolev <info@hondaworld.ru>
2025-01-22 10:04:23 +0300

    Очередной вариант

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -1,5 +1,7 @@
 package hw06pipelineexecution
 
+import "sync"
+
 type (
 	In  = <-chan interface{}
 	Out = In
@@ -8,38 +10,96 @@ type (
 
 type Stage func(in In) (out Out)
 
-func concurrentPipeline(in In, done In, stages ...Stage) Out {
-	for _, stage := range stages {
+func take(in In, done In, stage Stage) Out {
+	takeStream := make(Bi)
+	tempStream := make(Bi)
+
+	go func() {
+		defer close(takeStream)
+		//defer wg.Done()
+
 		select {
 		case <-done:
-			return done
-		default:
-			in = stage(in)
+			return
+		case takeStream <- stage(tempStream):
 		}
-	}
+	}()
 
-	return in
+	go func() {
+		defer close(tempStream)
+
+		for v := range in {
+			//wg.Add(1)
+			tempStream <- v
+		}
+	}()
+
+	return takeStream
 }
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
 	out := make(Bi)
-	//wg := sync.WaitGroup{}
+	outTemp := make(Bi)
+	temp := make(Bi)
+	wg := sync.WaitGroup{}
 	//
 	//wg.Add(1)
 
-	pipeline := concurrentPipeline(in, done, stages...)
+	//pipeline := concurrentPipeline(in, done, stages...)
+
+	//go func() {
+	//	defer close(outTemp)
+	//
+	//	select {
+	//	case <-done:
+	//		return
+	//	case temp <- outTemp:
+	//	}
+	//}()
 
 	go func() {
-		defer close(out)
+		wg.Wait()
+		//out <- temp
+	}()
 
-		for stage := range pipeline {
+	go func() {
+		defer wg.Done()
+		for _, stage := range stages {
+			//wg.Add(1)
+			//s := stage
+
+			//go func() {
+
+			//for {
 			select {
 			case <-done:
 				break
-			default:
-				out <- stage
+			case temp <- <-outTemp:
+			//case outTemp <- <-take(temp, done, stage):
+			case outTemp <- <-stage(temp):
+				//case temp <- outTemp:
+			}
+			//}
+			//}()
+		}
+	}()
+
+	go func() {
+		defer close(temp)
+
+		//for {
+		for v := range in {
+
+			select {
+			case <-done:
+				return
+			case temp <- v:
+				wg.Add(1)
+				//case temp <- outTemp:
 			}
 		}
+
+		//out <- temp
 	}()
 
 	//wg.Wait()
