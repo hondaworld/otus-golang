9af524954d9e199f62c684c5664ad92be8589349
Sergey Sobolev <info@hondaworld.ru>
2025-02-01 23:22:47 +0300

    HW7 is completed

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"errors"
+	"io"
+	"os"
+
+	"github.com/cheggaaa/pb/v3"
 )
 
 var (
@@ -10,6 +14,48 @@ var (
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	sourceFile, err := os.OpenFile(fromPath, os.O_RDONLY, 0)
+	if err != nil {
+		return ErrUnsupportedFile
+	}
+	defer sourceFile.Close()
+
+	sourceFileInfo, err := sourceFile.Stat()
+	if err != nil {
+		return err
+	}
+
+	if offset > sourceFileInfo.Size() {
+		return ErrOffsetExceedsFileSize
+	}
+
+	_, err = sourceFile.Seek(offset, io.SeekStart)
+	if err != nil {
+		return ErrOffsetExceedsFileSize
+	}
+
+	destinationFile, err := os.Create(toPath)
+	if err != nil {
+		return ErrUnsupportedFile
+	}
+	defer destinationFile.Close()
+
+	switch {
+	case limit == 0:
+		limit = sourceFileInfo.Size() - offset
+	case limit > sourceFileInfo.Size()-offset:
+		limit = sourceFileInfo.Size() - offset
+	}
+
+	bar := pb.Full.Start64(limit)
+	defer bar.Finish()
+
+	progressReader := bar.NewProxyReader(sourceFile)
+
+	_, err = io.CopyN(destinationFile, progressReader, limit)
+	if err != nil && !errors.Is(err, io.EOF) {
+		return err
+	}
+
 	return nil
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,143 @@
 package main
 
-import "testing"
+import (
+	"bytes"
+	"errors"
+	"io"
+	"log"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+const file1 = "testdata/input.txt"
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	t.Run("offset 0, limit 0", func(t *testing.T) {
+		file2, err := os.CreateTemp("", "test-*")
+		file3 := "testdata/out_offset0_limit0.txt"
+
+		if err != nil {
+			log.Fatalf("Failed to create temp file: %v", err)
+		}
+		defer os.Remove(file2.Name())
+
+		errCopy := Copy(file1, file2.Name(), 0, 0)
+
+		equal, err := filesAreEqual(file2.Name(), file3)
+
+		file2.Close()
+
+		require.Truef(t, equal, "files ars equal")
+		require.Nil(t, errCopy)
+		require.Nil(t, err)
+	})
+	t.Run("offset 0, limit 10", func(t *testing.T) {
+		file2, err := os.CreateTemp("", "test-*")
+		file3 := "testdata/out_offset0_limit10.txt"
+
+		if err != nil {
+			log.Fatalf("Failed to create temp file: %v", err)
+		}
+		defer os.Remove(file2.Name())
+		defer file2.Close()
+
+		errCopy := Copy(file1, file2.Name(), 0, 10)
+
+		equal, err := filesAreEqual(file2.Name(), file3)
+
+		require.Truef(t, equal, "files ars equal")
+		require.Nil(t, errCopy)
+		require.Nil(t, err)
+	})
+	t.Run("offset 6000, limit 1000", func(t *testing.T) {
+		file2, err := os.CreateTemp("", "test-*")
+		file3 := "testdata/out_offset6000_limit1000.txt"
+
+		if err != nil {
+			log.Fatalf("Failed to create temp file: %v", err)
+		}
+		defer os.Remove(file2.Name())
+		defer file2.Close()
+
+		errCopy := Copy(file1, file2.Name(), 6000, 1000)
+
+		equal, err := filesAreEqual(file2.Name(), file3)
+
+		require.Truef(t, equal, "files ars equal")
+		require.Nil(t, errCopy)
+		require.Nil(t, err)
+	})
+	t.Run("offset 10000, limit 1000", func(t *testing.T) {
+		file2, err := os.CreateTemp("", "test-*")
+
+		if err != nil {
+			log.Fatalf("Failed to create temp file: %v", err)
+		}
+		defer os.Remove(file2.Name())
+		defer file2.Close()
+
+		errCopy := Copy(file1, file2.Name(), 10000, 1000)
+
+		require.Truef(t, errors.Is(errCopy, ErrOffsetExceedsFileSize), "actual err - %v", errCopy)
+	})
+	t.Run("not file exists", func(t *testing.T) {
+		file1Wrong := "testdata1/input.txt"
+		file2, err := os.CreateTemp("", "test-*")
+
+		if err != nil {
+			log.Fatalf("Failed to create temp file: %v", err)
+		}
+		defer os.Remove(file2.Name())
+		defer file2.Close()
+
+		errCopy := Copy(file1Wrong, file2.Name(), 10000, 1000)
+
+		require.Truef(t, errors.Is(errCopy, ErrUnsupportedFile), "actual err - %v", errCopy)
+	})
+}
+
+func filesAreEqual(file1, file2 string) (bool, error) {
+	sourceFile1, err := os.Open(file1)
+	if err != nil {
+		return false, err
+	}
+	defer sourceFile1.Close()
+
+	sourceFile2, err := os.Open(file2)
+	if err != nil {
+		return false, err
+	}
+	defer sourceFile2.Close()
+
+	const bufSize = 4096
+	buf1 := make([]byte, bufSize)
+	buf2 := make([]byte, bufSize)
+
+	for {
+		n1, err1 := sourceFile1.Read(buf1)
+		n2, err2 := sourceFile2.Read(buf2)
+
+		if n1 != n2 || (err1 == io.EOF && err2 != io.EOF) || (err1 != io.EOF && err2 == io.EOF) {
+			return false, nil
+		}
+
+		if err1 == io.EOF && err2 == io.EOF {
+			break
+		}
+
+		if !bytes.Equal(buf1[:n1], buf2[:n2]) {
+			return false, nil
+		}
+
+		if err1 != nil && err1 != io.EOF {
+			return false, err1
+		}
+		if err2 != nil && err2 != io.EOF {
+			return false, err2
+		}
+	}
+
+	return true, nil
 }
