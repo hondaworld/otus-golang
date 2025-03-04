81f03cf76bc19d666a23f7d7a8ebb72b01fe5e9b
Sergey Sobolev <info@hondaworld.ru>
2025-03-04 23:38:00 +0300

    HW11 is completed

--- hw11_telnet_client/main.go
+++ hw11_telnet_client/main.go
@@ -1,6 +1,61 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
 func main() {
-	// Place your code here,
-	// P.S. Do not rush to throw context down, think think if it is useful with blocking operation?
+	timeout := flag.Duration("timeout", 10*time.Second, "Сетевой таймаут")
+	flag.Parse()
+
+	args := flag.Args()
+	if len(args) < 2 {
+		fmt.Println("Использование: go-telnet [--timeout=10s] host port")
+		os.Exit(1)
+	}
+
+	host := args[0]
+	port := args[1]
+
+	address := net.JoinHostPort(host, port)
+
+	client := NewTelnetClient(address, *timeout, os.Stdin, os.Stdout)
+
+	err := client.Connect()
+	if err != nil {
+		fmt.Printf("Ошибка подключения: %v\n", err)
+		os.Exit(1)
+	}
+	defer client.Close()
+
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM)
+
+	done := make(chan struct{})
+
+	go func() {
+		if err := client.Send(); err != nil {
+			fmt.Fprintf(os.Stderr, "Send error: %v\n", err)
+			close(done)
+		}
+	}()
+
+	go func() {
+		_ = client.Receive()
+		close(done)
+	}()
+
+	select {
+	case <-done: // completed sending or receiving
+		fmt.Println("Exiting due to EOF or send/receive completion.")
+	case <-sigCh: // caught signal
+	}
+
+	client.Close()
 }
--- hw11_telnet_client/telnet.go
+++ hw11_telnet_client/telnet.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"io"
+	"net"
 	"time"
 )
 
@@ -12,10 +13,47 @@ type TelnetClient interface {
 	Receive() error
 }
 
+type telnetClient struct {
+	address string
+	timeout time.Duration
+	in      io.ReadCloser
+	out     io.Writer
+	conn    net.Conn
+}
+
 func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
-	// Place your code here.
+	return &telnetClient{
+		address: address,
+		timeout: timeout,
+		in:      in,
+		out:     out,
+	}
+}
+
+func (t *telnetClient) Connect() error {
+	conn, err := net.DialTimeout("tcp", t.address, t.timeout)
+	if err != nil {
+		return err
+	}
+	t.conn = conn
 	return nil
 }
 
-// Place your code here.
-// P.S. Author's solution takes no more than 50 lines.
+func (t *telnetClient) Close() error {
+	if t.conn != nil {
+		return t.conn.Close()
+	}
+	return nil
+}
+
+// Send forwards data from the input (typically os.Stdin) to the connection.
+func (t *telnetClient) Send() error {
+	_, err := io.Copy(t.conn, t.in)
+	return err
+}
+
+// Receive forwards data from the connection to the output (typically os.Stdout).
+func (t *telnetClient) Receive() error {
+	_, err := io.Copy(t.out, t.conn)
+	return err
+}
